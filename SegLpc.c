1d46ed2d87369bc861ff850a46413441ee94ba95
juliomenaya <juliomenaya@gmail.com>
2016-02-28 19:58:14 -0600

    Falta meterse con matrices de tres dimensiones, lo demas creo que es correcto. Las partes comentadas eran para mis pruebas de escritorio

--- SegLpc.c
+++ SegLpc.c
@@ -1,26 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>//libreria para usar funciones de memoria dinamica
-
-/*SegLpc(float A[][1]){
-	float **x;//apuntador doble para asignar memoria
-	int i;
-	x = (float **)malloc(200*sizeof(float*));//4000 es un valor arbitrario solo para poder iniciar la reservacion de memoria
-	for (i=0;i<4000;i++) 
-		x[i] = (float*)malloc(1*sizeof(float)); 
-}*/
-
-void main(main){
-	float *x;//apuntador doble para asignar memoria
-	int i;
-	x = (float *)malloc(3*sizeof(float));//4000 es un valor arbitrario solo para poder iniciar la reservacion de memoria
-	for(i=0;i<10;i++){
-		printf("\nvalor x[%d] es %f\n",i,*(x+i));	
+#include<math.h>
+SegLpc(float A[][1]){
+	float *x,*y,tam,tam2;
+	float** segmen;
+	int i,j;
+	int reng;
+	reng=sizeof(A)/sizeof(float);//calcula el tamaño del vector de entrada 
+	
+	x = (float *)malloc(reng*sizeof(float));
+	y =(float*)malloc(reng*sizeof(float));//Matriz Y para realizar preenfasis
+	
+	tam=floor(reng/4);
+	tam2=ceil(tam/128);
+	segmen=(float**)calloc(4,sizeof(float*));
+	for(i=0;i<tam;i++){ //Inicializar la matriz segmen con ceros
+		segmen[i]=(float*)calloc(tam,sizeof(float));
+		
 	}
 	
-	for(i=0;i<10;i++){//Codigo para saber con que valores inicializa el arreglo
-	printf("\nDireccion x[%d] %p\n",i,(x+i));	
+	int ventana[4][tam2][128];
+	int autocor[4][tam2][9];
+	/*x[0]=10; //Codigo para saber si 
+	x[1]=3;
+	x[2]=4;
+	x[3]=7;
+	x[4]=9;
+	for(i=0;i<reng;i++){
+		y[i]=x[i]+((0.95)*(x[i]));
 	}
-	int tama=sizeof(x());//Codigo para saber si a pesar de ser memoria dinamiza la funcion sizeof me daria el tamaño total
-	printf("El tamano del arreglo es %d",tama);
+	
+	for(i=0;i<reng;i++){
+		printf("\nEl valor en la posicion %d es %f\n",i,*(y+i));
+		
+	}
+	*/
+}
+
+void main(main){
+	/*float *x,*y;//apuntador doble para asignar memoria
+	int i,j;
+	x = (float *)malloc(10*sizeof(float));//4000 es un valor arbitrario solo para poder iniciar la reservacion de memoria
+	  
+	y =(float*)malloc(10*sizeof(float));//Matriz Y para realizar preenfasis
+	
+*/
+	
 	return;
 }
