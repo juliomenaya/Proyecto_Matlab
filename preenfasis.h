7bb2d04d3ac1995c40de42dd821098826e74afb9
juliomenaya <juliomenaya@gmail.com>
2016-03-17 13:36:54 -0600

    Corrección del nombre de la función.

--- preenfasis.h
+++ preenfasis.h
@@ -1,6 +1,6 @@
 #include<stdio.h>
-void correlacion(float x[]);//prototipo de la funcion
-void correlacion(float x[]){//declaración de la funcion preenfasis
+void preensasis(float x[]);//prototipo de la funcion
+void preenfasis(float x[]){//declaración de la funcion preenfasis
 	int i;//contador del ciclo
 	for(i=1;i<16000;i++){//comienza el ciclo de preenfasis
 		x[i]+=-0.95*x[i-1];
