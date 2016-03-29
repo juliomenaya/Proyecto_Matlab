71319a2c6054ae9f73b9c46cc2ccd78a09707ffe
juliomenaya <juliomenaya@gmail.com>
2016-03-29 15:21:11 -0600

    Prueba de las funciones realizadas

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>//Biblioteca para usar la funcion rand y srand
 #include<time.h>//Biblioteca para obtener la hora del sistema
 #include"preenfasis.h" //cabecera para usar la funcion preenfasis
+#include "ventaneo.h"
 int main() {
 	int i; //contador
 	float signal[16000]; //Señal de entrada
@@ -10,18 +11,15 @@ int main() {
 	for (i = 0; i < 16000; i++){	//ciclo para que la señal tenga valores aleatorios
 		signal[i]=(float)(rand() % 101)/100;
 	}
-	
 	printf("\nAntes del filtro\n");
-	
-	for(i=0;i<10;i++){
+	for(i=0;i<100;i++){
 		printf("\nsignal[%d]=%.2f\n",i,signal[i]);
 	}
-	
 	preenfasis(signal);//llamada a la funcion preenfasis
-  
 	printf("\n Preenfasis\n");
 	for(i=0;i<10;i++){
 		printf("\nsignalP[%d]=%.2f\n",i,signal[i]);
 	}
+	ventaneo(signal,256,128);
 	return 0;
 }
